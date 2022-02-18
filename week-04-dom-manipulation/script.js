b7ea82d47dfffdd46e1479106d6592699ae8ecd2
JDFogel <jacksondfogel@gmail.com>
2022-02-17 21:08:23 -0500

    Week 4 Project

--- week-04-dom-manipulation/script.js
+++ week-04-dom-manipulation/script.js
@@ -14,7 +14,60 @@ function fillOutputs() {
   adjectiveOneOutput.innerText = adjectiveOne;
 
   // Get the output container element
-  const outputDiv = document.getElementById('output');
+  const outputDiv = document.getElementById('output1');
   // Make the output container element visible
   outputDiv.style.display = 'block';
+
+}
+
+function fillVerbOutput() {
+	
+	const verbOneInput = document.getElementById('verb-one-input');
+	const verbOne = verbOneInput.value
+		
+	const verbOneOutput = document.getElementById('verb-one-output');
+	verbOneOutput.innerText = verbOne;
+	
+	const output2Div = document.getElementById('output2');
+	output2Div.style.display = 'block';
+	
+}
+
+function fillAnimalOutput() {
+	
+	const animalOneInput = document.getElementById('animal-one-input');
+	const animalOne = animalOneInput.value
+		
+	const animalOneOutput = document.getElementById('animal-one-output');
+	animalOneOutput.innerText = animalOne;
+	
+	const output3Div = document.getElementById('output3');
+	output3Div.style.display = 'block';
+	
+}
+
+function fillAdjectiveTwoOutput() {
+	
+	const adjectiveTwoInput = document.getElementById('adjective-two-input');
+	const adjectiveTwo = adjectiveTwoInput.value
+		
+	const adjectiveTwoOutput = document.getElementById('adjective-two-output');
+	adjectiveTwoOutput.innerText = adjectiveTwo;
+	
+	const output4Div = document.getElementById('output4');
+	output4Div.style.display = 'block';
+	
+}
+
+function fillPlaceOutput() {
+	
+	const placeOneInput = document.getElementById('place-one-input');
+	const placeOne = placeOneInput.value
+		
+	const placeOneOutput = document.getElementById('place-one-output');
+	placeOneOutput.innerText = placeOne;
+	
+	const output5Div = document.getElementById('output5');
+	output5Div.style.display = 'block';
+	
 }
