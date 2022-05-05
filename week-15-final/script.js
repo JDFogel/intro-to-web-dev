1f354bd175123f84a3dc7e6420382331012a23e9
JDFogel <jacksondfogel@gmail.com>
2022-05-05 00:22:37 -0400

    Final Project Rough and Parallax Test

---
+++ week-15-final/script.js
@@ -0,0 +1,7 @@
+function sorryPop() {
+
+	const emailAddress = document.getElementById('email-input').value;
+
+	alert("Thanks! We'll save " + emailAddress + " for later when Chris sets up his mailing list!")
+
+}
